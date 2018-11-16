06b4d4ec0d39414f09107d9947228f2d81967730
Cheng Xing <cxing@google.com>
2018-11-16 14:30:00 -0800

    Updating generated API clients

--- pkg/client/clientset/versioned/typed/volumesnapshot/v1alpha1/fake/fake_volumesnapshot.go
+++ pkg/client/clientset/versioned/typed/volumesnapshot/v1alpha1/fake/fake_volumesnapshot.go
@@ -131,7 +131,7 @@ func (c *FakeVolumeSnapshots) DeleteCollection(options *v1.DeleteOptions, listOp
 // Patch applies the patch and returns the patched volumeSnapshot.
 func (c *FakeVolumeSnapshots) Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.VolumeSnapshot, err error) {
 	obj, err := c.Fake.
-		Invokes(testing.NewPatchSubresourceAction(volumesnapshotsResource, c.ns, name, data, subresources...), &v1alpha1.VolumeSnapshot{})
+		Invokes(testing.NewPatchSubresourceAction(volumesnapshotsResource, c.ns, name, pt, data, subresources...), &v1alpha1.VolumeSnapshot{})
 
 	if obj == nil {
 		return nil, err
