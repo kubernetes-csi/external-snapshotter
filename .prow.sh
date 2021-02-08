830300dddeeceebc606f37113868422f0757878d
ialidzhikov <i.alidjikov@gmail.com>
2021-02-08 18:57:23 +0200

    Fix release-tools/prow.sh for K8s 1.20
    Signed-off-by: ialidzhikov <i.alidjikov@gmail.com>

--- .prow.sh
+++ .prow.sh
@@ -3,6 +3,7 @@
 # This is specific to the release-2.1 branch and overrides the
 # version set in the prow config.
 export CSI_SNAPSHOTTER_VERSION=v2.1.2
+export CSI_PROW_DRIVER_VERSION=v1.4.0
 
 . release-tools/prow.sh
 
