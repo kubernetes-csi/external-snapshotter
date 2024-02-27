8c619462a423d940b7f67245037ed1194baf5931
Tiger Kaovilai <tkaovila@redhat.com>
2024-02-27 00:29:03 -0500

    add wait backoff
    Signed-off-by: Tiger Kaovilai <tkaovila@redhat.com>

--- pkg/utils/update.go
+++ pkg/utils/update.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"context"
 	"errors"
+	"math"
+	"time"
 
 	crdv1alpha1 "github.com/kubernetes-csi/external-snapshotter/client/v7/apis/volumegroupsnapshot/v1alpha1"
 	crdv1 "github.com/kubernetes-csi/external-snapshotter/client/v7/apis/volumesnapshot/v1"
@@ -10,8 +12,19 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 )
+
+// backoff from main.go
+const retryFactor = 1.5
+const initialDuration = 100 * time.Millisecond
+
+var backoff = wait.Backoff{
+	Duration: initialDuration,
+	Factor:   retryFactor,
+	Steps:    math.MaxInt32, // effectively no limit until the timeout is reached
+}
 
 // Remove one or more finalizers from an object
 // if finalizers is not empty, only the specified finalizers will be removed
@@ -21,24 +34,29 @@ func UpdateRemoveFinalizers[O metav1.Object](
 	updateFunc func(context.Context, O, metav1.UpdateOptions) (O, error),
 	getFunc func(context.Context, string, metav1.GetOptions) (O, error),
 	finalizers ...string) (O, error) {
-	for success := false; !success; {
+	err := wait.ExponentialBackoff(backoff, func() (bool, error) {
 		object.SetFinalizers(RemoveStrings(object.GetFinalizers(), finalizers...))
 		updatedObject, err := updateFunc(context.TODO(), object, metav1.UpdateOptions{})
 		if err != nil {
 			if apierrors.IsConflict(err) {
+				// conflict, object is out of date, get the latest version
 				object, err = getFunc(context.TODO(), object.GetName(), metav1.GetOptions{})
 				if err != nil {
-					return object, err
+					return false, err
 				}
 				// retry removing finalizers
-				continue
-			} else {
-				// return error if it's not a conflict
-				return object, err
+				return false, nil
 			}
+			// other errors
+			return false, err
 		}
-		success = true
+		// success
 		object = updatedObject
+		return true, nil
+	})
+	// }
+	if err != nil {
+		return object, err
 	}
 	if len(object.GetFinalizers()) == 0 {
 		// to satisfy some tests that requires nil rather than []string{}
