9461f70bc19f593d0979d3ee417447560056c168
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2024-05-17 10:48:35 +0200

    Remove duplicate validation logic from webhook
    This patch removes the logic to admit VolumeSnapshots,
    VolumeSnapshotContents, VolumeGroupSnapshots and
    VolumeGroupSnapshotContents in the validation webhook.
    
    That logic is already implemented via CEL expressions (see #1073)
    
    The logic to admit VolumeSnapshotClasses and VolumeGroupSnapshotClasses
    is still implemented in the webhook and avoids having multiple default
    classes for the same CSI Driver.

--- pkg/validation-webhook/groupsnapshot.go
+++ pkg/validation-webhook/groupsnapshot.go
@@ -18,7 +18,6 @@ package webhook
 
 import (
 	"fmt"
-	"reflect"
 
 	volumegroupsnapshotv1alpha1 "github.com/kubernetes-csi/external-snapshotter/client/v7/apis/volumegroupsnapshot/v1alpha1"
 	groupsnapshotlisters "github.com/kubernetes-csi/external-snapshotter/client/v7/listers/volumegroupsnapshot/v1alpha1"
@@ -30,10 +29,6 @@ import (
 )
 
 var (
-	// GroupSnapshotV1Alpha1GVR is GroupVersionResource for v1alpha1 VolumeGroupSnapshots
-	GroupSnapshotV1Alpha1GVR = metav1.GroupVersionResource{Group: volumegroupsnapshotv1alpha1.GroupName, Version: "v1alpha1", Resource: "volumegroupsnapshots"}
-	// GroupSnapshotContentV1Apha1GVR is GroupVersionResource for v1alpha1 VolumeGroupSnapshotContents
-	GroupSnapshotContentV1Apha1GVR = metav1.GroupVersionResource{Group: volumegroupsnapshotv1alpha1.GroupName, Version: "v1alpha1", Resource: "volumegroupsnapshotcontents"}
 	// GroupSnapshotClassV1Apha1GVR is GroupVersionResource for v1alpha1 VolumeGroupSnapshotClasses
 	GroupSnapshotClassV1Apha1GVR = metav1.GroupVersionResource{Group: volumegroupsnapshotv1alpha1.GroupName, Version: "v1alpha1", Resource: "volumegroupsnapshotclasses"}
 )
@@ -54,8 +49,7 @@ func NewGroupSnapshotAdmitter(lister groupsnapshotlisters.VolumeGroupSnapshotCla
 
 // Add a label {"added-label": "yes"} to the object
 func (a groupSnapshotAdmitter) Admit(ar v1.AdmissionReview) *v1.AdmissionResponse {
-	klog.V(2).Info("admitting volumegroupsnapshots volumegroupsnapshotcontents " +
-		"or volumegroupsnapshotclasses")
+	klog.V(2).Info("admitting volumegroupsnapshotclasses")
 
 	reviewResponse := &v1.AdmissionResponse{
 		Allowed: true,
@@ -66,37 +60,12 @@ func (a groupSnapshotAdmitter) Admit(ar v1.AdmissionReview) *v1.AdmissionRespons
 	if !(ar.Request.Operation == v1.Update || ar.Request.Operation == v1.Create) {
 		return reviewResponse
 	}
-	isUpdate := ar.Request.Operation == v1.Update
 
 	raw := ar.Request.Object.Raw
 	oldRaw := ar.Request.OldObject.Raw
 
 	deserializer := codecs.UniversalDeserializer()
 	switch ar.Request.Resource {
-	case GroupSnapshotV1Alpha1GVR:
-		groupSnapshot := &volumegroupsnapshotv1alpha1.VolumeGroupSnapshot{}
-		if _, _, err := deserializer.Decode(raw, nil, groupSnapshot); err != nil {
-			klog.Error(err)
-			return toV1AdmissionResponse(err)
-		}
-		oldGroupSnapshot := &volumegroupsnapshotv1alpha1.VolumeGroupSnapshot{}
-		if _, _, err := deserializer.Decode(oldRaw, nil, oldGroupSnapshot); err != nil {
-			klog.Error(err)
-			return toV1AdmissionResponse(err)
-		}
-		return decideGroupSnapshotV1Alpha1(groupSnapshot, oldGroupSnapshot, isUpdate)
-	case GroupSnapshotContentV1Apha1GVR:
-		groupSnapContent := &volumegroupsnapshotv1alpha1.VolumeGroupSnapshotContent{}
-		if _, _, err := deserializer.Decode(raw, nil, groupSnapContent); err != nil {
-			klog.Error(err)
-			return toV1AdmissionResponse(err)
-		}
-		oldGroupSnapContent := &volumegroupsnapshotv1alpha1.VolumeGroupSnapshotContent{}
-		if _, _, err := deserializer.Decode(oldRaw, nil, oldGroupSnapContent); err != nil {
-			klog.Error(err)
-			return toV1AdmissionResponse(err)
-		}
-		return decideGroupSnapshotContentV1Alpha1(groupSnapContent, oldGroupSnapContent, isUpdate)
 	case GroupSnapshotClassV1Apha1GVR:
 		groupSnapClass := &volumegroupsnapshotv1alpha1.VolumeGroupSnapshotClass{}
 		if _, _, err := deserializer.Decode(raw, nil, groupSnapClass); err != nil {
@@ -110,58 +79,11 @@ func (a groupSnapshotAdmitter) Admit(ar v1.AdmissionReview) *v1.AdmissionRespons
 		}
 		return decideGroupSnapshotClassV1Alpha1(groupSnapClass, oldGroupSnapClass, a.lister)
 	default:
-		err := fmt.Errorf("expect resource to be %s, %s, or %s, but found %v",
-			GroupSnapshotV1Alpha1GVR, GroupSnapshotContentV1Apha1GVR,
+		err := fmt.Errorf("expect resource to be %s, but found %v",
 			GroupSnapshotClassV1Apha1GVR, ar.Request.Resource)
 		klog.Error(err)
 		return toV1AdmissionResponse(err)
 	}
-}
-
-func decideGroupSnapshotV1Alpha1(groupSnapshot, oldGroupSnapshot *volumegroupsnapshotv1alpha1.VolumeGroupSnapshot, isUpdate bool) *v1.AdmissionResponse {
-	reviewResponse := &v1.AdmissionResponse{
-		Allowed: true,
-		Result:  &metav1.Status{},
-	}
-
-	if isUpdate {
-		// if it is an UPDATE and oldGroupSnapshot is valid, check immutable fields
-		if err := checkGroupSnapshotImmutableFieldsV1Alpha1(groupSnapshot, oldGroupSnapshot); err != nil {
-			reviewResponse.Allowed = false
-			reviewResponse.Result.Message = err.Error()
-			return reviewResponse
-		}
-	}
-	// Enforce strict validation for CREATE requests. Immutable checks don't apply for CREATE requests.
-	// Enforce strict validation for UPDATE requests where old is valid and passes immutability check.
-	if err := ValidateV1Alpha1GroupSnapshot(groupSnapshot); err != nil {
-		reviewResponse.Allowed = false
-		reviewResponse.Result.Message = err.Error()
-	}
-	return reviewResponse
-}
-
-func decideGroupSnapshotContentV1Alpha1(groupSnapcontent, oldGroupSnapcontent *volumegroupsnapshotv1alpha1.VolumeGroupSnapshotContent, isUpdate bool) *v1.AdmissionResponse {
-	reviewResponse := &v1.AdmissionResponse{
-		Allowed: true,
-		Result:  &metav1.Status{},
-	}
-
-	if isUpdate {
-		// if it is an UPDATE and oldGroupSnapcontent is valid, check immutable fields
-		if err := checkGroupSnapshotContentImmutableFieldsV1Alpha1(groupSnapcontent, oldGroupSnapcontent); err != nil {
-			reviewResponse.Allowed = false
-			reviewResponse.Result.Message = err.Error()
-			return reviewResponse
-		}
-	}
-	// Enforce strict validation for all CREATE requests. Immutable checks don't apply for CREATE requests.
-	// Enforce strict validation for UPDATE requests where old is valid and passes immutability check.
-	if err := ValidateV1Alpha1GroupSnapshotContent(groupSnapcontent); err != nil {
-		reviewResponse.Allowed = false
-		reviewResponse.Result.Message = err.Error()
-	}
-	return reviewResponse
 }
 
 func decideGroupSnapshotClassV1Alpha1(groupSnapClass, oldGroupSnapClass *volumegroupsnapshotv1alpha1.VolumeGroupSnapshotClass, lister groupsnapshotlisters.VolumeGroupSnapshotClassLister) *v1.AdmissionResponse {
@@ -199,56 +121,4 @@ func decideGroupSnapshotClassV1Alpha1(groupSnapClass, oldGroupSnapClass *volumeg
 	}
 
 	return reviewResponse
-}
-
-func checkGroupSnapshotImmutableFieldsV1Alpha1(groupSnapshot, oldGroupSnapshot *volumegroupsnapshotv1alpha1.VolumeGroupSnapshot) error {
-	if groupSnapshot == nil {
-		return fmt.Errorf("VolumeGroupSnapshot is nil")
-	}
-	if oldGroupSnapshot == nil {
-		return fmt.Errorf("old VolumeGroupSnapshot is nil")
-	}
-
-	source := groupSnapshot.Spec.Source
-	oldSource := oldGroupSnapshot.Spec.Source
-
-	if !reflect.DeepEqual(source.Selector, oldSource.Selector) {
-		return fmt.Errorf("Spec.Source.Selector is immutable but was changed from %s to %s", oldSource.Selector, source.Selector)
-	}
-	if !reflect.DeepEqual(source.VolumeGroupSnapshotContentName, oldSource.VolumeGroupSnapshotContentName) {
-		return fmt.Errorf("Spec.Source.VolumeGroupSnapshotContentName is immutable but was changed from %s to %s", strPtrDereference(oldSource.VolumeGroupSnapshotContentName), strPtrDereference(source.VolumeGroupSnapshotContentName))
-	}
-
-	return nil
-}
-
-func checkGroupSnapshotContentImmutableFieldsV1Alpha1(groupSnapcontent, oldGroupSnapcontent *volumegroupsnapshotv1alpha1.VolumeGroupSnapshotContent) error {
-	if groupSnapcontent == nil {
-		return fmt.Errorf("VolumeGroupSnapshotContent is nil")
-	}
-	if oldGroupSnapcontent == nil {
-		return fmt.Errorf("old VolumeGroupSnapshotContent is nil")
-	}
-
-	source := groupSnapcontent.Spec.Source
-	oldSource := oldGroupSnapcontent.Spec.Source
-
-	if !reflect.DeepEqual(source.GroupSnapshotHandles, oldSource.GroupSnapshotHandles) {
-		return fmt.Errorf("Spec.Source.GroupSnapshotHandles is immutable but was changed from %s to %s", oldSource.GroupSnapshotHandles, source.GroupSnapshotHandles)
-	}
-	if !reflect.DeepEqual(source.VolumeHandles, oldSource.VolumeHandles) {
-		return fmt.Errorf("Spec.Source.VolumeHandles is immutable but was changed from %v to %v", oldSource.VolumeHandles, source.VolumeHandles)
-	}
-
-	ref := groupSnapcontent.Spec.VolumeGroupSnapshotRef
-	oldRef := oldGroupSnapcontent.Spec.VolumeGroupSnapshotRef
-
-	if ref.Name != oldRef.Name {
-		return fmt.Errorf("Spec.VolumeGroupSnapshotRef.Name is immutable but was changed from %s to %s", oldRef.Name, ref.Name)
-	}
-	if ref.Namespace != oldRef.Namespace {
-		return fmt.Errorf("Spec.VolumeGroupSnapshotRef.Namespace is immutable but was changed from %s to %s", oldRef.Namespace, ref.Namespace)
-	}
-
-	return nil
 }
--- pkg/validation-webhook/snapshot.go
+++ pkg/validation-webhook/snapshot.go
@@ -18,7 +18,6 @@ package webhook
 
 import (
 	"fmt"
-	"reflect"
 
 	volumesnapshotv1 "github.com/kubernetes-csi/external-snapshotter/client/v7/apis/volumesnapshot/v1"
 	storagelisters "github.com/kubernetes-csi/external-snapshotter/client/v7/listers/volumesnapshot/v1"
@@ -30,10 +29,6 @@ import (
 )
 
 var (
-	// SnapshotV1GVR is GroupVersionResource for v1 VolumeSnapshots
-	SnapshotV1GVR = metav1.GroupVersionResource{Group: volumesnapshotv1.GroupName, Version: "v1", Resource: "volumesnapshots"}
-	// SnapshotContentV1GVR is GroupVersionResource for v1 VolumeSnapshotContents
-	SnapshotContentV1GVR = metav1.GroupVersionResource{Group: volumesnapshotv1.GroupName, Version: "v1", Resource: "volumesnapshotcontents"}
 	// SnapshotContentV1GVR is GroupVersionResource for v1 VolumeSnapshotContents
 	SnapshotClassV1GVR = metav1.GroupVersionResource{Group: volumesnapshotv1.GroupName, Version: "v1", Resource: "volumesnapshotclasses"}
 )
@@ -54,7 +49,7 @@ func NewSnapshotAdmitter(lister storagelisters.VolumeSnapshotClassLister) Snapsh
 
 // Add a label {"added-label": "yes"} to the object
 func (a admitter) Admit(ar v1.AdmissionReview) *v1.AdmissionResponse {
-	klog.V(2).Info("admitting volumesnapshots or volumesnapshotcontents")
+	klog.V(2).Info("admitting volumesnapshotclasses")
 
 	reviewResponse := &v1.AdmissionResponse{
 		Allowed: true,
@@ -65,37 +60,12 @@ func (a admitter) Admit(ar v1.AdmissionReview) *v1.AdmissionResponse {
 	if !(ar.Request.Operation == v1.Update || ar.Request.Operation == v1.Create) {
 		return reviewResponse
 	}
-	isUpdate := ar.Request.Operation == v1.Update
 
 	raw := ar.Request.Object.Raw
 	oldRaw := ar.Request.OldObject.Raw
 
 	deserializer := codecs.UniversalDeserializer()
 	switch ar.Request.Resource {
-	case SnapshotV1GVR:
-		snapshot := &volumesnapshotv1.VolumeSnapshot{}
-		if _, _, err := deserializer.Decode(raw, nil, snapshot); err != nil {
-			klog.Error(err)
-			return toV1AdmissionResponse(err)
-		}
-		oldSnapshot := &volumesnapshotv1.VolumeSnapshot{}
-		if _, _, err := deserializer.Decode(oldRaw, nil, oldSnapshot); err != nil {
-			klog.Error(err)
-			return toV1AdmissionResponse(err)
-		}
-		return decideSnapshotV1(snapshot, oldSnapshot, isUpdate)
-	case SnapshotContentV1GVR:
-		snapcontent := &volumesnapshotv1.VolumeSnapshotContent{}
-		if _, _, err := deserializer.Decode(raw, nil, snapcontent); err != nil {
-			klog.Error(err)
-			return toV1AdmissionResponse(err)
-		}
-		oldSnapcontent := &volumesnapshotv1.VolumeSnapshotContent{}
-		if _, _, err := deserializer.Decode(oldRaw, nil, oldSnapcontent); err != nil {
-			klog.Error(err)
-			return toV1AdmissionResponse(err)
-		}
-		return decideSnapshotContentV1(snapcontent, oldSnapcontent, isUpdate)
 	case SnapshotClassV1GVR:
 		snapClass := &volumesnapshotv1.VolumeSnapshotClass{}
 		if _, _, err := deserializer.Decode(raw, nil, snapClass); err != nil {
@@ -109,57 +79,11 @@ func (a admitter) Admit(ar v1.AdmissionReview) *v1.AdmissionResponse {
 		}
 		return decideSnapshotClassV1(snapClass, oldSnapClass, a.lister)
 	default:
-		err := fmt.Errorf("expect resource to be %s, %s, or %s, but found %v",
-			SnapshotV1GVR, SnapshotContentV1GVR, SnapshotClassV1GVR, ar.Request.Resource)
+		err := fmt.Errorf("expect resource to be %s, but found %v",
+			SnapshotClassV1GVR, ar.Request.Resource)
 		klog.Error(err)
 		return toV1AdmissionResponse(err)
 	}
-}
-
-func decideSnapshotV1(snapshot, oldSnapshot *volumesnapshotv1.VolumeSnapshot, isUpdate bool) *v1.AdmissionResponse {
-	reviewResponse := &v1.AdmissionResponse{
-		Allowed: true,
-		Result:  &metav1.Status{},
-	}
-
-	if isUpdate {
-		// if it is an UPDATE and oldSnapshot is valid, check immutable fields
-		if err := checkSnapshotImmutableFieldsV1(snapshot, oldSnapshot); err != nil {
-			reviewResponse.Allowed = false
-			reviewResponse.Result.Message = err.Error()
-			return reviewResponse
-		}
-	}
-	// Enforce strict validation for CREATE requests. Immutable checks don't apply for CREATE requests.
-	// Enforce strict validation for UPDATE requests where old is valid and passes immutability check.
-	if err := ValidateV1Snapshot(snapshot); err != nil {
-		reviewResponse.Allowed = false
-		reviewResponse.Result.Message = err.Error()
-	}
-	return reviewResponse
-}
-
-func decideSnapshotContentV1(snapcontent, oldSnapcontent *volumesnapshotv1.VolumeSnapshotContent, isUpdate bool) *v1.AdmissionResponse {
-	reviewResponse := &v1.AdmissionResponse{
-		Allowed: true,
-		Result:  &metav1.Status{},
-	}
-
-	if isUpdate {
-		// if it is an UPDATE and oldSnapcontent is valid, check immutable fields
-		if err := checkSnapshotContentImmutableFieldsV1(snapcontent, oldSnapcontent); err != nil {
-			reviewResponse.Allowed = false
-			reviewResponse.Result.Message = err.Error()
-			return reviewResponse
-		}
-	}
-	// Enforce strict validation for all CREATE requests. Immutable checks don't apply for CREATE requests.
-	// Enforce strict validation for UPDATE requests where old is valid and passes immutability check.
-	if err := ValidateV1SnapshotContent(snapcontent); err != nil {
-		reviewResponse.Allowed = false
-		reviewResponse.Result.Message = err.Error()
-	}
-	return reviewResponse
 }
 
 func decideSnapshotClassV1(snapClass, oldSnapClass *volumesnapshotv1.VolumeSnapshotClass, lister storagelisters.VolumeSnapshotClassLister) *v1.AdmissionResponse {
@@ -204,56 +128,4 @@ func strPtrDereference(s *string) string {
 		return "<nil string pointer>"
 	}
 	return *s
-}
-
-func checkSnapshotImmutableFieldsV1(snapshot, oldSnapshot *volumesnapshotv1.VolumeSnapshot) error {
-	if snapshot == nil {
-		return fmt.Errorf("VolumeSnapshot is nil")
-	}
-	if oldSnapshot == nil {
-		return fmt.Errorf("old VolumeSnapshot is nil")
-	}
-
-	source := snapshot.Spec.Source
-	oldSource := oldSnapshot.Spec.Source
-
-	if !reflect.DeepEqual(source.PersistentVolumeClaimName, oldSource.PersistentVolumeClaimName) {
-		return fmt.Errorf("Spec.Source.PersistentVolumeClaimName is immutable but was changed from %s to %s", strPtrDereference(oldSource.PersistentVolumeClaimName), strPtrDereference(source.PersistentVolumeClaimName))
-	}
-	if !reflect.DeepEqual(source.VolumeSnapshotContentName, oldSource.VolumeSnapshotContentName) {
-		return fmt.Errorf("Spec.Source.VolumeSnapshotContentName is immutable but was changed from %s to %s", strPtrDereference(oldSource.VolumeSnapshotContentName), strPtrDereference(source.VolumeSnapshotContentName))
-	}
-
-	return nil
-}
-
-func checkSnapshotContentImmutableFieldsV1(snapcontent, oldSnapcontent *volumesnapshotv1.VolumeSnapshotContent) error {
-	if snapcontent == nil {
-		return fmt.Errorf("VolumeSnapshotContent is nil")
-	}
-	if oldSnapcontent == nil {
-		return fmt.Errorf("old VolumeSnapshotContent is nil")
-	}
-
-	source := snapcontent.Spec.Source
-	oldSource := oldSnapcontent.Spec.Source
-
-	if !reflect.DeepEqual(source.VolumeHandle, oldSource.VolumeHandle) {
-		return fmt.Errorf("Spec.Source.VolumeHandle is immutable but was changed from %s to %s", strPtrDereference(oldSource.VolumeHandle), strPtrDereference(source.VolumeHandle))
-	}
-	if !reflect.DeepEqual(source.SnapshotHandle, oldSource.SnapshotHandle) {
-		return fmt.Errorf("Spec.Source.SnapshotHandle is immutable but was changed from %s to %s", strPtrDereference(oldSource.SnapshotHandle), strPtrDereference(source.SnapshotHandle))
-	}
-
-	if preventVolumeModeConversion {
-		if oldSnapcontent.Spec.SourceVolumeMode != nil && snapcontent.Spec.SourceVolumeMode != nil && !reflect.DeepEqual(snapcontent.Spec.SourceVolumeMode, oldSnapcontent.Spec.SourceVolumeMode) {
-			return fmt.Errorf("Spec.SourceVolumeMode is immutable but was changed from %v to %v", *oldSnapcontent.Spec.SourceVolumeMode, *snapcontent.Spec.SourceVolumeMode)
-		} else if oldSnapcontent.Spec.SourceVolumeMode == nil && snapcontent.Spec.SourceVolumeMode != nil {
-			return fmt.Errorf("Spec.SourceVolumeMode is immutable but was changed from nil to %v", *snapcontent.Spec.SourceVolumeMode)
-		} else if oldSnapcontent.Spec.SourceVolumeMode != nil && snapcontent.Spec.SourceVolumeMode == nil {
-			return fmt.Errorf("Spec.SourceVolumeMode is immutable but was changed from %v to nil", *oldSnapcontent.Spec.SourceVolumeMode)
-		}
-	}
-
-	return nil
 }
